b843773257d403625560d89864af0af69e46094b
Dan Graham <dan@saucelabs.com>
2017-02-22 11:38:21 -0800

    Completed auto-updater lifecycle
    Uses electron-log instead of console.log so we can see the logs in a log-file. Added update-downloaded and error events.

--- app/main/auto-updater.js
+++ app/main/auto-updater.js
@@ -6,22 +6,32 @@ function connectAutoUpdater (win) {
   autoUpdater.logger = log;
   autoUpdater.logger.transports.file.level = "info";
 
-  console.log('Looking for updates');
+  log('Looking for updates');
   autoUpdater.checkForUpdates();
 
-  autoUpdater.on('checking-for-update', function () {
-    console.log('Checking for an update!');
+  autoUpdater.on('checking-for-update', () => {
+    log('Checking for an update!');
     win.webContents.send('checking-for-update');
   });
 
-  autoUpdater.on('update-not-available', function () {
-    console.log('Update is available');
+  autoUpdater.on('update-not-available', () => {
+    log('Update is not available');
     win.webContents.send('update-not-available');
   });
 
-  autoUpdater.on('update-available', function () {
-    console.log('Update is available');
+  autoUpdater.on('update-available', () => {
+    log('Update is available');
     win.webContents.send('update-available');
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    log('Update downloaded');
+    win.webContents.send('update-downloaded');
+  });
+
+  autoUpdater.on('error', () => {
+    log('Update error');
+    win.webContents.send('update-error');
   });
 }
 
